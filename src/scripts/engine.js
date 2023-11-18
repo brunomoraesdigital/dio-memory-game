0e0b2c8ba99a9fb23b3f2d4abd2a7a512a69dbb0
Bruno Moraes <brunomoraes.dev@gmail.com>
2023-11-18 11:27:28 -0300

    projeto dio finalizado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -36,6 +36,20 @@ function handleClick() {
     if(openCards.length == 2) {
         setTimeout(checkMatch, 500);
     }
+    console.log(openCards);
 }
 
-function checkMatch(){}
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Parabéns! Você Venceu!!!");
+        
+    }
+}
