c6897e4c3115882360161bd847302a58ab1609fb
Bruno Moraes <brunomoraes.dev@gmail.com>
2023-11-17 22:48:56 -0300

    171123.2248

---
+++ src/scripts/engine.js
@@ -0,0 +1,41 @@
+const emojis = [
+    "🐶",
+    "🐶",
+    "🐱",
+    "🐱",
+    "🐹",
+    "🐹",
+    "🐮",
+    "🐮",
+    "🐷",
+    "🐷",
+    "🐸",
+    "🐸",
+    "🐭",
+    "🐭",
+    "🐵",
+    "🐵"
+];
+let openCards= [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    } 
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){}
